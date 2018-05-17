db16d31be74f534c0ea5bdeb625153afe4d4df43
Sam <swheeler111@gmail.com>
2018-05-17 17:54:49 -0400

    excption for testing

--- tap_campaignmonitor/streams.py
+++ tap_campaignmonitor/streams.py
@@ -68,6 +68,7 @@ def sync(context):
 
 def call_stream_full(context, stream):
     if stream == 'campaigns':
+        raise Exception
         response = context.client.GET(stream='campaigns')
         records_to_write = json.loads(response.content)
         write_records(stream, records_to_write)
